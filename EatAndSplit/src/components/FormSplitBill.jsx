ff89c63c6ddb904de57343f5e3ce0d1c348fb32e
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-06 21:11:12 +0300

    Implemented the split bill functionality

--- EatAndSplit/src/components/FormSplitBill.jsx
+++ EatAndSplit/src/components/FormSplitBill.jsx
@@ -1,29 +1,50 @@
 /* eslint-disable react/prop-types */
-import Button from "./Button"
+import { useState } from "react";
+import Button from "./Button";
 
-function FormSplitBill({selectedFriend}) {
-    return (
-        <form className='form-split-bill'>
-            <h2>Split a bill with {selectedFriend.name}</h2>
-            
-            <label>💰 Bill value</label>
-            <input type='text'></input>
+function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, seWhoIsPaying] = useState("user");
 
-            <label>🙍‍♀️ Your expenses</label>
-            <input type='text'></input>
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
-            <label>👭 {selectedFriend.name}`s expense</label>
-            <input type='text' disabled></input>
+      <label>💰 Bill value</label>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      ></input>
 
-            <label>Who is paying the bill</label>
-            <select>
-                <option value='user'>You</option>
-                <option value='friend'>{selectedFriend.name}</option>
-            </select>
+      <label>🙍‍♀️ Your expenses</label>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      ></input>
 
-            <Button>Split bill</Button>
-        </form>
-    )
+      <label>👭 {selectedFriend.name}`s expense</label>
+      <input type="text" disabled value={paidByFriend}></input>
+
+      <label>Who is paying the bill</label>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => seWhoIsPaying(e.target.value)}
+      >
+        <option value="user">You</option>
+        <option value="friend">{selectedFriend.name}</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
 }
 
-export default FormSplitBill
+export default FormSplitBill;
